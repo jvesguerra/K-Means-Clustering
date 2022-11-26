dcf26bdf2c40603c00741e0e62d0e7b986089c84
jvesguerra <jvesguerra@up.edu.ph>
2022-11-26 15:18:08 +0800

    get data from user

--- a.py
+++ a.py
@@ -6,6 +6,7 @@ options = []
 selected = []   # the user input
 max_selection = 2
 
+# Part 1. GET DATA
 # open csv file then save data in rows to data array
 with open("./data/wine.csv",'r') as file:
     csvfile = csv.reader(file)
@@ -15,14 +16,22 @@ with open("./data/wine.csv",'r') as file:
 options = data[0];
 
 # PRINT MENU
-for j in range(0,max_selection):
-    print("MENU")
+while len(selected) != max_selection:
+    print("MENU \n")
     for i in range(0, len(options)):
         print("[",i,"] ",options[i])
 
     choice = int(input("Enter attribute [n]: "))
-    selected.append(options[choice])
+
+    # selected option must be unique
+    if options[choice] in selected:
+        print("Cannot add selected attribute\n")
+    else:
+        print("Successfully added",options[choice],"\n")
+        selected.append(options[choice])
 
 n_cluster = int(input("Enter n cluster: "))
 selected.append(n_cluster)
 print(selected)
+
+# Part 2.
