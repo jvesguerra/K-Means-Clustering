ff991a4c4cd7dd81abe35a01fdb6ef0aacb8c167
jvesguerra <jvesguerra@up.edu.ph>
2022-11-26 22:32:53 +0800

    Feat: Get class count

--- a.py
+++ a.py
@@ -13,7 +13,7 @@ k_centroids = []
 rand_k = []
 dist_list1 = []
 dist_list2 = []
-
+k = 2
 # Part 1. GET DATA
 # open csv file then save data in rows to data array
 # with open("./data/wine.csv",'r') as file:
@@ -56,49 +56,76 @@ for i in range(0,data_length):
     xy = [data[i][index_arr[0]],data[i][index_arr[1]]]
     xy_arr.append(xy)
 
-set_dp = copy.deepcopy(xy_arr)
+print(xy_arr)
+# xy_arr is the original datapoints
+# datapoints would be use for the loop
+
+# LOOP HERE
+
 # get k data points
-while len(k_centroids) != max_selection:
+datapoints = copy.deepcopy(xy_arr)
+while len(k_centroids) != k:
     random_k = random.randint(1,(data_length-1))
     rand_k.append(random_k)
     k_centroids.append(xy_arr[random_k])
 
 kc_length = len(k_centroids)
-xy_length = len(xy_arr)
+xy_length = len(datapoints)
+
+# FOR TESTING
+k_centroids.clear()
+rand_k.clear()
+rand_k = [4,6]
+k_centroids = [[2,2],[4,4]]
+# CHANGE LATER
 
 # get the distances
 for kc in range(0,kc_length):    # loop k centroids
     for xy in range(1,xy_length):          # loop data points
         eudistance = 0
         for index in range(0,2):
-            x = float(xy_arr[xy][index])
+            x = float(datapoints[xy][index])
             c = float(k_centroids[kc][index])
             eudistance = ((x - c)**2) + eudistance
         eudistance = math.sqrt(eudistance)
-        xy_arr[xy].append(eudistance)
-
-# index 2 is distance from first k centroid
-# index 3 is distance from second k centroid
+        datapoints[xy].append(eudistance)
 
 # get which datapoint is close then group by k
 for xy in range(1,xy_length):
     for kc in range(0,kc_length):
         if kc == 0:
-            min = xy_arr[xy][kc]
-            group = set_dp[rand_k[kc]]
+            min = datapoints[xy][kc]
+            group = xy_arr[rand_k[kc]]
         else:
-            if xy_arr[xy][kc] < min:
-                min = xy_arr[xy][kc]
-                group = set_dp[rand_k[kc]]
-    xy_arr[xy].append(group)
-
-for i in range(0,2):
-    print(set_dp[rand_k[i]]," ",end="")
-print()
-
-for xy in range(1,xy_length):
-    print(xy_arr[xy])   
+            if datapoints[xy][kc] < min:
+                min = datapoints[xy][kc]
+                group = xy_arr[rand_k[kc]]
+    datapoints[xy].append(group)
 
 # group = 2 + k
-
 group_ind = 2 + kc_length # index of the groupings
+
+for xy in range(1,xy_length):
+    print(datapoints[xy])   
+
+# PART 3. Solve for new centroid
+
+# count class
+class_count = {}
+for i in range(0,k):
+    for j in range(1,xy_length):
+        if j not in rand_k:
+            if datapoints[j][group_ind] == xy_arr[rand_k[i]]:
+                if class_count.get(i) is None:
+                    class_count[i] = 1
+                else:
+                    class_count[i] += 1
+
+print(class_count)
+
+
+# reset values
+rand_k.clear()
+k_centroids.clear()
+datapoints.clear()
+
